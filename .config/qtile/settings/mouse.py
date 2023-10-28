ed9940e19a409b2154c92b7da3a72781b791e8d5
= <=>
2023-10-27 18:04:46 -0600

    fix(qtile): change config

--- .config/qtile/settings/mouse.py
+++ .config/qtile/settings/mouse.py
@@ -10,11 +10,11 @@ mouse = [
         lazy.window.set_position_floating(),
         start=lazy.window.get_position()
     ),
-    Drag(
-        [alt],
-        "Button3",
-        lazy.window.set_size_floating(),
-        start=lazy.window.get_size()
-    ),
-    Click([alt], "Button2", lazy.window.bring_to_front())
+    # Drag(
+    #     [alt],
+    #     "Button3",
+    #     lazy.window.set_size_floating(),
+    #     start=lazy.window.get_size()
+    # ),
+    Click([alt], "Button1", lazy.window.bring_to_front())
 ]
